ecd0e84cb12cb34fc1549ef3769b6988c0952d1f
jakparent <70171524+jakparent@users.noreply.github.com>
2021-02-09 12:36:20 -0500

    Update Update.php

--- LAMPAPI/Update.php
+++ LAMPAPI/Update.php
@@ -1,18 +1,40 @@
 <?php
 	$inData = getRequestInfo();
 	
+	// userfirstname : user first name
+	// userlastname : user last name
 	$password = $inData["password"];
-	$userId = $inData["userId"];
-	/*
+	$userid = $inData["userid"];
+	$userfirstname = $inData["userfirstname"];
+	$userlastname = $inData["userlastname"];
+	$login = $inData["login"];
+	
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	echo "hullo\n";
+	//echo "hullo\n";
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		$sql = "Update Users Set Password= '$password' WHERE ID = $userId";
+		if($password)
+		{
+			$sql = "UPDATE Users SET Password= '$password' WHERE ID= $userid";
+			echo "password updated\n";
+		}
+		else if($userfirstname)
+		{
+			$sql = "UPDATE Users SET firstName= '$userfirstname' WHERE ID= $userid";
+		}
+		else if($userlastname)
+		{
+			$sql = "UPDATE Users SET lastName= '$userlastname' WHERE ID= $userid";
+		}
+		else if($login)
+		{
+			$sql = "UPDATE Users SET login= '$login' WHERE ID= $userid";
+		}
+		//$sql = "Update Users Set Password= '$password' WHERE ID = $userId";
 		//$sql = "UPDATE Users SET Password='WeLoveCOP4331' WHERE ID=1";
 		//$sql = "Update Users (UserId,Password) SET (" . $userId . ",'" . $password . "')";
 		if( $result = $conn->query($sql) != TRUE )
@@ -21,7 +43,7 @@
 		}
 		$conn->close();
 	}
-	*/
+	
 	returnWithError("");
 	
 	function getRequestInfo()
