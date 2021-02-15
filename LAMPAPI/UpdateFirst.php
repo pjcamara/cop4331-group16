e669483c4ba7de520459682c86c74d8cc7ad83d6
jakparent <70171524+jakparent@users.noreply.github.com>
2021-02-15 16:35:28 -0500

    Add files via upload

---
+++ LAMPAPI/UpdateFirst.php
@@ -0,0 +1,48 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$phonenumber = $inData["phonenumber"];
+	$userid = $inData["userid"];
+	$confirstname = $inData["confirstname"];
+	$conlastname = $inData["conlastname"];
+	
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		
+			$sql = "UPDATE Contacts SET firstName= '$confirstname' WHERE ID= $userid";
+		
+	
+
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
